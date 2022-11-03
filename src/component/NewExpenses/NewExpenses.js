3cf0d30fb7c37c980dec351c3b0209fdc200f35d
Khan <893949@cognizant.com>
2022-11-03 14:12:13 +0530

    first commit

---
+++ src/component/NewExpenses/NewExpenses.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './NewExpenses.css'
+import ExpensesForm from "./ExpensesForm";
+
+const NewExpenses = ()=> {
+    return(
+        <div className="new-expense">
+            <ExpensesForm></ExpensesForm>
+
+        </div>
+    )
+}
+export default NewExpenses;
